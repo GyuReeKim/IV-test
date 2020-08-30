28cc213509305f184f4ee17dba8eb40086330f32
GyuReeKim <starkim96@naver.com>
2020-08-30 23:24:30 +0900

    [+] Grid

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
 class Game extends Component {
   render() {
+    const containerStyle = {
+      textAlign: 'center'
+    };
+
     const imageStyle = {
-      width: '45vw'
+      width: '95%'
     };
 
     return (
-      <div>
+      <div style={containerStyle}>
         <h1>시작페이지</h1>
         <p>이미지</p>
-        <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
-        <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+        <Grid container justify="center">
+          <Grid item xs={6}>
+            <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          </Grid>
+          <Grid item xs={6}>
+            <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          </Grid>
+        </Grid>
+        <Grid container justify="center">
+          <Grid item xs={6}>
+            <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          </Grid>
+          <Grid item xs={6}>
+            <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          </Grid>
+        </Grid>
       </div>
     );
   }
