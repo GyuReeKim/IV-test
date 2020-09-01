113a135ddabe242ebbbf3c2510bf01b03e179db5
GyuReeKim <starkim96@naver.com>
2020-09-01 21:57:34 +0900

    [+] Add new component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
-const Start = () => {
+const Start = props => {
   const itemStyle = {
     margin: '2vw',
     maxWidth: '45vw',
-    maxHeight: '25vh',
+    maxHeight: '25vh'
   };
   
   const imageStyle = {
@@ -16,22 +16,46 @@ const Start = () => {
     borderRadius: '10px'
   };
 
+  const selectMap = (n) => {
+    props.setMapSelection(n)
+  };
+
   return (
     <div>
       <Grid container justify="center">
         <Grid item style={itemStyle}>
-          <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          <img
+            src={process.env.PUBLIC_URL + '/images/test.png'}
+            alt="test"
+            style={imageStyle}
+            onClick={() => selectMap(1)}
+          />
         </Grid>
         <Grid item style={itemStyle}>
-          <img src={process.env.PUBLIC_URL + '/images/test2.jpg'} alt="test2" style={imageStyle} />
+          <img
+            src={process.env.PUBLIC_URL + '/images/test2.jpg'}
+            alt="test2"
+            style={imageStyle}
+            onClick={() => selectMap(2)}
+          />
         </Grid>
       </Grid>
       <Grid container justify="center">
         <Grid item style={itemStyle}>
-          <img src={process.env.PUBLIC_URL + '/images/test.png'} alt="test" style={imageStyle} />
+          <img
+            src={process.env.PUBLIC_URL + '/images/test.png'}
+            alt="test"
+            style={imageStyle}
+            onClick={() => selectMap(3)}
+          />
         </Grid>
         <Grid item style={itemStyle}>
-          <img src={process.env.PUBLIC_URL + '/images/test2.jpg'} alt="test2" style={imageStyle} />
+          <img
+            src={process.env.PUBLIC_URL + '/images/test2.jpg'}
+            alt="test2"
+            style={imageStyle}
+            onClick={() => selectMap(4)}
+          />
         </Grid>
       </Grid>
     </div>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Start from '../components/Start';
 
 const Game = () => {
   const containerStyle = {
-    textAlign: 'center'
+    textAlign: 'center',
+    width: '100vw'
   };
 
-  return (
-    <div style={containerStyle}>
-      <h1>시작페이지</h1>
-      <p>이미지</p>
-      <Start />
-    </div>
-  );
+  const test = '/images/test3.jpg'
+
+  const backgroundImage1 = {
+    backgroundImage: `url(${process.env.PUBLIC_URL + test})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+  };
+
+  const [mapSelection, setMapSelection] = useState(0);
+
+  if (mapSelection === 1) {
+    return (
+      <div style={containerStyle}>
+        <h1>시작페이지</h1>
+        <div style={backgroundImage1}>          
+          <p>이미지</p>
+          <p>{mapSelection}</p>
+        </div>
+      </div>
+    );
+  } else if (mapSelection === 2) {
+    return (
+      <div style={containerStyle}>
+        <h1>시작페이지</h1>
+        <p>이미지</p>
+        <p>{mapSelection}</p>
+      </div>
+    );
+  } else if (mapSelection === 3) {
+    return (
+      <div style={containerStyle}>
+        <h1>시작페이지</h1>
+        <p>이미지</p>
+        <p>{mapSelection}</p>
+      </div>
+    );
+  } else if (mapSelection === 4) {
+    return (
+      <div style={containerStyle}>
+        <h1>시작페이지</h1>
+        <p>이미지</p>
+        <p>{mapSelection}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div style={containerStyle}>
+        <h1>시작페이지</h1>
+        <p>이미지</p>
+        <p>{mapSelection}</p>
+        <Start
+          mapSelection={mapSelection}
+          setMapSelection={setMapSelection}
+        />
+      </div>
+    );
+  }
 }
 
 export default Game;
