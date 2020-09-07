55e6c3417577b96f6717a2ffe63ba096776a80c6
GyuReeKim <starkim96@naver.com>
2020-09-07 18:48:25 +0900

    [+] Add result page

---
+++ src/pages/Result.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Character1 from '../components/characters/Character1';
+import Character2 from '../components/characters/Character2';
+import Character3 from '../components/characters/Character3';
+
+const Result = () => {
+  return (
+    <div>
+      <h1>Result</h1>
+    </div>
+  );
+}
+
+export default Result;
